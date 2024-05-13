aea636994fa4d0a32fbc5be6f28b5a4f2b4e4c3b
Ellorahhh <serhat.karakas234@gmail.com>
2024-05-13 16:01:25 +0300

    Preview Layout fixed

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -1,5 +1,5 @@
 const PublicLayout = ({ children }) => {
-  return <div className="h-full dark:bg-[#1F1F1F]">{children}</div>;
+  return <div className="h-4xl w-full dark:bg-[#1F1F1F]">{children}</div>;
 };
 
 export default PublicLayout;
